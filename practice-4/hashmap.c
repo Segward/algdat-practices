0b7117dba4545585febbd99546b6e7e5a40d2828
segward <gustavhaverstad@gmail.com>
2025-10-03 01:38:48 +0200

    reworked hashmap with dump

--- practice-4/hashmap.c
+++ practice-4/hashmap.c
@@ -3,107 +3,117 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct node node_t;
+typedef struct node_s node_t;
 
-typedef struct node{
+typedef struct node_s {
   char *key;
   char *value;
   node_t *next;
 } node_t;
 
-void node_init(node_t *node, char *key, void *value) {
-  node->key = key;
-  node->value = value;
-  node->next = NULL;
-}
-
 typedef struct {
   node_t **nodes;
-  size_t size;
   size_t capacity;
 } hashmap_t;
 
-int hashmap_init(hashmap_t *map, size_t capacity) {
+hashmap_t *hashmap_init(size_t capacity) {
+  hashmap_t *map = (hashmap_t *)malloc(sizeof(hashmap_t));
+  if (!map)
+    return NULL;
+
   map->nodes = (node_t **)malloc(sizeof(node_t *) * capacity);
   if (!map->nodes)
-    return -1;
+    return NULL;
 
-  map->size = 0;
   map->capacity = capacity;
-  return 0;
+  return map;
 }
 
-int hash(char *key, size_t capacity) {
-  int sum = 0, factor = 0;
+unsigned int hash(char *key, size_t capacity) {
+  unsigned int sum = 0;
   for (int i = 0; key[i] != '\0'; i++) {
-    sum = (sum % capacity + ((int)key[i] * factor) % capacity) % capacity;
-    factor = ((factor % __INT32_MAX__) * 31 % __INT32_MAX__) % __INT32_MAX__;
+    sum += sum * 37 + key[i];
   }
 
-  return sum;
+  return sum % capacity;
 }
 
-int hashmap_insert(hashmap_t *map, char *key, char *value) {
-  int index = hash(key, map->capacity);
-  node_t *new_node = (node_t *)malloc(sizeof(node_t));
-  if (!new_node)
-    return -1;
+node_t *node_init(char *key, char *value, unsigned int *index) {
+  node_t *node = (node_t *)malloc(sizeof(node_t));
+  if (!node)
+    return NULL;
 
-  node_init(new_node, key, value);
-  if (map->nodes[index] == NULL) {
-    map->nodes[index] = new_node;
-    return 0;
-  }
-
-  new_node->next = map->nodes[index];
-  map->nodes[index] = new_node;
-  return 0;
+  node->key = key;
+  node->value = value;
+  node->next = NULL;
+  return node;
 }
 
-node_t *hashmap_get(hashmap_t *map, char *key) {
-  int index = hash(key, map->capacity);
+void hashmap_insert(hashmap_t *map, char *key, char *value) {
+  unsigned int index = hash(key, map->capacity);
   node_t *node = map->nodes[index];
+  if (node == NULL) {
+    map->nodes[index] = node_init(key, value, &index);
+    return;
+  }
 
-  node_t *head = NULL, *tail = NULL;
+  node_t *prev = NULL;
   while (node != NULL) {
     if (strcmp(node->key, key) == 0) {
-      node_t *copy = (node_t *)malloc(sizeof(node_t));
-      if (!copy)
-        return NULL;
-
-      node_init(copy, node->key, node->value);
-      if (head == NULL) {
-        head = copy;
-        tail = copy;
-      } else {
-        tail->next = copy;
-        tail = copy;
-      }
+      node->value = value;
+      return;
     }
+
+    prev = node;
+    node = node->next;
+  }
+
+  prev->next = node_init(key, value, &index);
+}
+
+char *hashmap_get(hashmap_t *map, char *key) {
+  unsigned int index = hash(key, map->capacity);
+  node_t *node = map->nodes[index];
+  if (node == NULL)
+    return NULL;
+
+  while (node != NULL) {
+    if (strcmp(node->key, key) == 0)
+      return node->value;
 
     node = node->next;
   }
 
-  return head;
+  return NULL;
+}
+
+void hashmap_dump(hashmap_t *map) {
+  for (size_t i = 0; i < map->capacity; i++) {
+    node_t *node = map->nodes[i];
+    if (node == NULL)
+      continue;
+
+    printf("[%zu]: ", i);
+    while (node != NULL) {
+      printf("(%s: %s) -> ", node->key, node->value);
+      node = node->next;
+    }
+
+    printf("NULL\n");
+  }
 }
 
 int main(int argc, char *argv[]) {
-  hashmap_t *map = (hashmap_t *)malloc(sizeof(hashmap_t));
-  if (hashmap_init(map, 1000) != 0) {
-    fprintf(stderr, "Failed to initialize hashmap\n");
-    return -1;
-  }
-
-  hashmap_insert(map, "test", "Alice");
-  hashmap_insert(map, "test2", "Bob");
-  hashmap_insert(map, "test3", "Charlie");
-  hashmap_insert(map, "test2", "David");
-
-  node_t *test2 = hashmap_get(map, "test2");
-  while (test2 != NULL) {
-    printf("Key: %s, Value: %s\n", test2->key, test2->value);
-    test2 = test2->next;
-  }
+  hashmap_t *map = hashmap_init(3);
+  hashmap_insert(map, "temp1", "Alice");
+  hashmap_insert(map, "temp2", "Bob");
+  hashmap_insert(map, "temp3", "Charlie");
+  hashmap_insert(map, "temp4", "David");
+  hashmap_insert(map, "temp5", "Eve");
+  hashmap_dump(map);
+  
+  char *value = hashmap_get(map, "temp4");
+  printf("Value for 'temp4': %s\n", value ? value : "Not found");
 
   return 0;
 }
